08c8ded2685863b3b4b5732375887c8475b757d6
Eshan V Gopal <eshanvgopal@gmail.com>
2021-08-08 14:55:21 +0530

    routes commit

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-    console.log(req.url,req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>First Landing</title></head>');
-    res.write('<body>Hello I am Node.js server!!!:)</body>');
-    res.write('</html>');
-    res.end();
-});
+const routes = require("./routes")
+
+const server = http.createServer(routes);
 
 server.listen(3000);
