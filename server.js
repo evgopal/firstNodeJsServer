8eb6edc7e95c0c0b53381165ae4ed69263248a67
Eshan V Gopal <eshanvgopal@gmail.com>
2021-08-12 00:07:55 +0530

    added Routes

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    console.log('In the middleware')
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-}) 
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!!')
